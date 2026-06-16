ff0c409791cb8e4309e387972e1670e9b948c3a2
Kapil Gautam <gautamkapil9080@gmail.com>
2026-06-16 19:56:26 +0545

    Added Simple

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express=require("express");
 const app=express();
+const ejsMate=require("ejs-mate");
 app.set("view engine","ejs");
 const path= require("path");
 app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
 
 let port=8080;
 
-app.get("/",(req,res)=>{
-    res.render("one");
+app.get("/",(req,res)=>{ // Route Route: 
+    res.render("listings/index");
+})
+
+app.get("/login",(req,res)=>{
+    res.send("Login");
+})
+app.get("/signup",(req,res)=>{
+    res.send("Signup");
 })
 
 app.listen(port,()=>{
