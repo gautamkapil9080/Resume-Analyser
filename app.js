46cd8ccee8b411db430a1ce947b580aa4eeeca18
Kapil Gautam <gautamkapil9080@gmail.com>
2026-06-18 17:30:48 +0545

    Added Function of Hashing

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.engine("ejs", ejsMate);
 const mongoose=require("mongoose"); 
 const connectionDb=require("./model/connection");
 const userModel=require("./model/user");
+const bcrypt=require("bcrypt");
 connectionDb();
+app.use(express.urlencoded({ extended: true }));
 
 let port=8080;
 
@@ -19,8 +21,34 @@ app.get("/",(req,res)=>{ // Route Route:
 app.get("/login",(req,res)=>{
     res.render("listings/login");
 })
+app.post("/login/submmit",async (req,res)=>{
+    const  {email,password}=req.body;
+    const passwordDb=await userModel.findOne({
+        email:email
+    });
+    const passwordfromDb=passwordDb.password;
+    const answer=await bcrypt.compare(password,passwordfromDb);
+    if(answer){
+        res.send("Login");
+    }
+    else{
+        res.send("Failed again");
+    }
+});
 app.get("/signup",(req,res)=>{
     res.render("listings/create");
+})
+app.post('/signup/submmit',async (req,res)=>{
+        const  {userName,email,password}=req.body;
+    const hashedPassword=await bcrypt.hash(password,10);
+    const user=new userModel({        // Documetns
+        userName:userName,
+        password:hashedPassword,
+        email:email
+    })
+    await user.save(user);
+    console.log(user);
+    res.send("Saved")
 })
 
 app.listen(port,()=>{
