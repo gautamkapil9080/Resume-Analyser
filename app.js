31586ef7f14d90e5f28ccaab61d85b9ac478a0b6
Kapil Gautam <gautamkapil9080@gmail.com>
2026-06-22 00:35:34 +0545

    Created Authentications and Authorizations

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ const mongoose=require("mongoose");
 const connectionDb=require("./model/connection");
 const userModel=require("./model/user");
 const bcrypt=require("bcrypt");
+const session=require("express-session");
 connectionDb();
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret:"mySuperSecretKey2026"
+}));
 
 let port=8080;
 
 app.get("/",(req,res)=>{ // Route Route: 
     res.render("listings/index");
+})
+app.get("/temp",(req,res)=>{
+    if(req.session.userId){
+        res.send("Can access");
+    }
+    else{
+        res.redirect("/login");
+    }
+    console.log(req.session.userId);
 })
 
 app.get("/login",(req,res)=>{
@@ -29,12 +42,15 @@ app.post("/login/submmit",async (req,res)=>{
     const passwordfromDb=passwordDb.password;
     const answer=await bcrypt.compare(password,passwordfromDb);
     if(answer){
+        req.session.userId=passwordDb._id;
         res.send("Login");
+        // console.log(req.session.userid);
     }
     else{
         res.send("Failed again");
     }
 });
+
 app.get("/signup",(req,res)=>{
     res.render("listings/create");
 })
@@ -49,6 +65,17 @@ app.post('/signup/submmit',async (req,res)=>{
     await user.save(user);
     console.log(user);
     res.send("Saved")
+})
+
+app.get("/logout",(req,res)=>{
+    if(req.session.userId){
+         req.session.userId=null;
+    res.send("Succesfully Logout")
+    }
+    else{
+        res.send("You have already Logout");
+    }
+   
 })
 
 app.listen(port,()=>{
